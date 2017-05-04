2fa1e244dcc0ef2b57ec14c377495f7172892d3f
Diana Vashti <diana.vashti@gmail.com>
2017-05-04 15:29:33 -0700

    Finding browser compatible github logo

--- client/bioBody.js
+++ client/bioBody.js
@@ -13,7 +13,7 @@ export default class BioBody extends Component {
           <p> Hi, I'm Di! </p>
           <div>
             <a href="https://github.com/DianaVashti">
-              <img src="images/githubmark.png" height="42" width="42" />
+              <img src="images/octocat.png" height="42" width="42" />
             </a>
             <a href="https://www.linkedin.com/in/diana-vashti-633b48135/">
               <img src="images/linkedin.png" height="47" width="47" />
